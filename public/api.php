185247b8c2fbe8e61f9d5d998b2624739457ca60
Baptiste Clavié <clavie.b@gmail.com>
2017-09-19 22:47:25 +0200

    Forcefully load a .env

--- public/api.php
+++ public/api.php
@@ -6,10 +6,8 @@ use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-if (file_exists(__DIR__ . '/../.env')) {
-    $dotenv = new Dotenv;
-    $dotenv->load(__DIR__ . '/../.env');
-}
+$dotenv = new Dotenv;
+$dotenv->load(__DIR__ . '/../.env');
 
 $kernel = new Kernel;
 
