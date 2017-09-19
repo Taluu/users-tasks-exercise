4f1636ee609fe25e23ce13cef0edef9fb9201c9d
Baptiste Clavié <clavie.b@gmail.com>
2017-09-19 18:13:04 +0200

    Use .env files

--- public/api.php
+++ public/api.php
@@ -1,9 +1,15 @@
 <?php
 namespace Test\One;
 
+use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists(__DIR__ . '/../.env')) {
+    $dotenv = new Dotenv;
+    $dotenv->load(__DIR__ . '/../.env');
+}
 
 $kernel = new Kernel;
 
